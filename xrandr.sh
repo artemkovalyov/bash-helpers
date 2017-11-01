1d0d31fe73f8f8a0d20bcb186368ec879a5970b0
Artem Kovalyov <artem.kovalyov@gmail.com>
2017-11-01 12:29:55 +0100

    Initial commit for script switching display modes when using two monitor configuration

---
+++ xrandr.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# My first script
+
+cat << _EOF_
+This script switches XrandR configs on my laptop
+_EOF_
+
+XRNDR="xrandr"
+CONNECTED_OUTPUTS=$($XRNDR | grep " connected" | grep -o "\w*-[0-9]\b")
+
+if [ $(id -u) = "0" ]; then
+    echo "superuser"
+fi
+
+if [ $(id -u) != "0" ]; then
+    echo "You must be the superuser to run this script" >&2
+    #exit 1
+fi
+
+debug() {
+  echo "Debug INFO: $1, $2"
+}
+
+set_layout() {
+  echo "${@:3}"
+  case $1 in
+    clone)
+      $XRNDR --output $3 --mode 1920x1080
+    ;;
+    above)
+      $XRNDR --output $3 --mode 1920x1080 --above $2
+    ;;
+    left)
+      $XRNDR --output $3 --mode 1920x1080 --left-of $2
+    ;;
+    right)
+      $XRNDR --output $3 --mode 1920x1080 --right-of $2
+    ;;
+    below)
+      $XRNDR --output $3 --mode 1920x1080 --below $2
+    ;;
+    off)
+      for i in ${@:3}; do
+        $XRNDR --output $i --off
+      done
+    ;;
+
+
+  esac
+}
+
+debug $CONNECTED_OUTPUTS
+
+set_layout $1 $CONNECTED_OUTPUTS
