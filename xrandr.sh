b021d5ff8960412732a683c4152cd955e469aea3
Artem Kovalyov <artem.kovalyov@gmail.com>
2017-11-01 12:37:01 +0100

    Added some comments and reminders

--- xrandr.sh
+++ xrandr.sh
@@ -6,6 +6,7 @@ This script switches XrandR configs on my laptop
 _EOF_
 
 XRNDR="xrandr"
+# select all the active outputs from xrandr log
 CONNECTED_OUTPUTS=$($XRNDR | grep " connected" | grep -o "\w*-[0-9]\b")
 
 if [ $(id -u) = "0" ]; then
@@ -21,6 +22,7 @@ debug() {
   echo "Debug INFO: $1, $2"
 }
 
+# 1 pararm - layout config, 2 param - array of active ouputs
 set_layout() {
   echo "${@:3}"
   case $1 in
@@ -40,6 +42,7 @@ set_layout() {
       $XRNDR --output $3 --mode 1920x1080 --below $2
     ;;
     off)
+      #switch off all the displays except the primary one (shift to the third param and loop through the outputs)
       for i in ${@:3}; do
         $XRNDR --output $i --off
       done
